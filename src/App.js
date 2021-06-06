b8efaf7953a9397ee6355214cad87ceb8693262c
mila <liudmyla@teemyco.com>
2021-06-06 14:39:28 +0300

    add gradient text overlay

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import TextOverlay from './components/TextLayer';
 import classes from './styles/app.scss';
 
 function App() {
-  return <div className={classes.masonery}>
-    <TextOverlay 
-      url="https://images.unsplash.com/photo-1517886703796-91acf7e02e1b" 
-      wrapperClassname={classes.masonery__item}
-      type="transparentTextLayer"
-    >
-        <h4 className={classes.masonery__header}>Header of the text overlay</h4>
-        <p className={classes.masonery__description}> Long and detailed description of the image content</p>
-    </TextOverlay>
-  </div>;
+  return (
+    <div className={classes.masonery}>
+      <TextOverlay 
+        url="https://images.unsplash.com/photo-1517886703796-91acf7e02e1b" 
+        wrapperClassname={classes.masonery__item}
+        type="transparentTextLayer"
+      >
+          <h4 className={classes.masonery__header}>Transparent Text Layer</h4>
+          <p className={classes.masonery__description}> Long and detailed description of the image content</p>
+      </TextOverlay>
+      <TextOverlay 
+        url="https://images.unsplash.com/photo-1517886703796-91acf7e02e1b" 
+        wrapperClassname={classes.masonery__item}
+        type="gradientTextLayer"
+      >
+          <h4 className={classes.masonery__header}>Gradient Text Layer</h4>
+          <p className={classes.masonery__description}> Long and detailed description of the image content</p>
+      </TextOverlay>
+  </div>
+  );
 };
 
 export default App;
